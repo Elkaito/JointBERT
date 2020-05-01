38972f7194d7c14d233180b78ab3eb268989147a
Kai Tanaka <kai-tanaka@gmx.de>
2020-05-01 14:42:09 +0200

    added args

--- main.py
+++ main.py
@@ -89,6 +89,7 @@ def main(args):
         trainer.dev_dataset = dev_dataset
         trainer.test_dataset = test_dataset
         # Change model output_layer and labels
+        trainer.args = args
         trainer.bert_config = trainer.config_class.from_pretrained(args.model_name_or_path, finetuning_task=args.task)
         trainer.model = trainer.model_class(trainer.bert_config, args, get_intent_labels(args), get_slot_labels(args))
         trainer.model.num_intent_labels = len(get_intent_labels(args))
