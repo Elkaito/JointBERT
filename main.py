e8d00dccf6ea528d215e4966daa768c6e76c7a7c
Kai Tanaka <kai-tanaka@gmx.de>
2020-05-02 14:00:18 +0200

    deleted commented outcode

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from data_loader import load_and_cache_examples
 import time
 from datetime import timedelta
 from model import JointBERT, IntentClassifier, SlotClassifier, PRETRAINED_MODEL_MAP
+
 def main(args):
 
     init_logger()
@@ -142,104 +143,3 @@ if __name__ == '__main__':
 
     args.model_name_or_path = MODEL_PATH_MAP[args.model_type]
     main(args)
-
-"""
-# Case 1: If pre task is atis, train on atis first, then main task
-    if args.pre_task and args.pre_task == "atis":
-
-        main_task = args.task
-        pre_task = args.pre_task
-        # Pretrain task on full dataset
-        args.task = pre_task
-        pre_train_dataset = load_and_cache_examples(args, tokenizer, mode="train")
-        pre_dev_dataset = load_and_cache_examples(args, tokenizer, mode="dev")
-        pre_test_dataset = load_and_cache_examples(args, tokenizer, mode="test")
-
-        trainer = Trainer(args, pre_train_dataset, pre_dev_dataset, pre_test_dataset)
-
-        if args.do_train:
-            #Pre train on pre_task 1
-            trainer.train()
-
-            if args.pre_task_2: # Pre train on task 2 if specified
-
-                trainer.load_model() # load params from task 1
-                pre_task_2 = args.pre_task_2
-                # Pretrain on full dataset
-                args.task = pre_task_2
-                pre2_train_dataset = load_and_cache_examples(args, tokenizer, mode="train")
-                pre2_dev_dataset = load_and_cache_examples(args, tokenizer, mode="dev")
-                pre2_test_dataset = load_and_cache_examples(args, tokenizer, mode="test")
-
-                trainer.train_dataset = pre2_train_dataset
-                trainer.dev_dataset = pre2_dev_dataset
-                trainer.test_dataset = pre2_test_dataset
-                trainer.train()
-
-            trainer.load_model()
-
-            # Train on main_task
-            args.task = main_task
-            args.data_dir = "./few-shot"
-            train_dataset = load_and_cache_examples(args, tokenizer, mode="train")
-            dev_dataset = load_and_cache_examples(args, tokenizer, mode="dev")
-            test_dataset = load_and_cache_examples(args, tokenizer, mode="test")
-
-            trainer.train_dataset = train_dataset
-            trainer.dev_dataset = dev_dataset
-            trainer.test_dataset = test_dataset
-            trainer.train()
-
-        if args.do_eval:
-            trainer.load_model()
-            trainer.evaluate("test")
-
-    # CASE 2: Pre task not atis
-    elif args.pre_task:
-        main_task = args.task
-        pre_task = args.pre_task
-        # Load pre-task data
-        args.task = pre_task
-        pre_train_dataset = load_and_cache_examples(args, tokenizer, mode="train")
-        pre_dev_dataset = load_and_cache_examples(args, tokenizer, mode="dev")
-        pre_test_dataset = load_and_cache_examples(args, tokenizer, mode="test")
-
-        trainer = Trainer(args, pre_train_dataset, pre_dev_dataset, pre_test_dataset)
-        model_dir = args.model_dir
-        args.model_dir = "snips_model"
-        trainer.load_model()
-        # Back to original task and model dir
-        args.model_dir = model_dir
-        args.task = main_task
-        # data_dir for few-shot
-        args.data_dir = "./few-shot"
-        # Get main task data
-        train_dataset = load_and_cache_examples(args, tokenizer, mode="train")
-        dev_dataset = load_and_cache_examples(args, tokenizer, mode="dev")
-        test_dataset = load_and_cache_examples(args, tokenizer, mode="test")
-
-        # trainer = Trainer(args, train_dataset, dev_dataset, test_dataset)
-        # trainer.train()
-
-        trainer.train_dataset = train_dataset
-        trainer.dev_dataset = dev_dataset
-        trainer.test_dataset = test_dataset
-        # Change trainer config to main_task
-        trainer.args = args
-      #  trainer.bert_config = trainer.config_class.from_pretrained(args.model_name_or_path, finetuning_task=args.task)
-        trainer.intent_label_lst = get_intent_labels(args)
-        trainer.slot_label_lst = get_slot_labels(args)
-        #trainer.model = trainer.model_class(trainer.bert_config, args, get_intent_labels(args), get_slot_labels(args))
-        trainer.model.num_intent_labels = len(get_intent_labels(args))
-        trainer.model.num_slot_labels = len(get_slot_labels(args))
-       # trainer.model.bert = PRETRAINED_MODEL_MAP[args.model_type].from_pretrained(args.model_name_or_path, config=trainer.bert_config)
-        trainer.model.intent_classifier.linear = nn.Linear(trainer.bert_config.hidden_size, len(get_intent_labels(args)))
-        trainer.model.slot_classifier.linear = nn.Linear(trainer.bert_config.hidden_size, len(get_slot_labels(args)))
-        trainer.model.to(trainer.device)
-        trainer.train()
-
-
-        if args.do_eval:
-            trainer.load_model()
-            trainer.evaluate("test")
-"""
