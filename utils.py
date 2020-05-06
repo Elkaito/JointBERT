decb8d68f6be9cd453fea8d30695fe5b04fe7eb2
Kai <tanaka@ukp.informatik.tu-darmstadt.de>
2020-05-06 18:54:59 +0200

    added seed settings

--- utils.py
+++ utils.py
@@ -44,6 +44,9 @@ def set_seed(args):
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
+    torch.random.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = True
+
     if not args.no_cuda and torch.cuda.is_available():
         torch.cuda.manual_seed_all(args.seed)
 
